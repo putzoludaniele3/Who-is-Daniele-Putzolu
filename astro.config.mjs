b0948c1b0a76395e2b84897555c0538c4934aa7c
putzoludaniele3 <daniele.putzolu.0103@gmail.com>
2024-10-04 16:30:22 +0200

    ritorno daniele putzolu

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/Who-is-padrepio/',
+  base: '/Who-is-Daniele-Putzolu/',
   integrations: [tailwind()]
 });
