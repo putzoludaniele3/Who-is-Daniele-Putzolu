7277dc773935935b8949c873031a7f3491024bf6
putzoludaniele3 <daniele.putzolu.0103@gmail.com>
2024-10-04 16:23:17 +0200

    padre pio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/Who-is-Daniele-Putzolu/',
+  base: '/Who-is-padrepio/',
   integrations: [tailwind()]
 });
